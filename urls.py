15eb7655cd08a3e0561d63d1853002e123267a40
Daniel Mizyrycki <mzdaniel@glidelink.net>
2017-04-03 15:35:31 -0700

    Improve url parsing. Make main url the top level page

--- urls.py
+++ urls.py
@@ -21,6 +21,6 @@ from cfp.views import ProposalCreate
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^main/', TemplateView.as_view(template_name='index.html'), name="main"),
-    url(r'^cfp/', ProposalCreate.as_view(success_url='/main'), name="cfp"),
+    url(r'^cfp($|/)', ProposalCreate.as_view(success_url='/'), name="cfp"),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="main"),
 ]
